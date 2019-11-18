c8bc1495819a2d2eb5519ee4e1b633f880681da8
Daniel <daniel.szarek98@gmail.com>
2019-11-18 23:51:54 +0100

    Deleted unused imports #HAPP-34

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -1,9 +1,6 @@
 from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated
 
 
 class UserListView(generics.ListCreateAPIView):
