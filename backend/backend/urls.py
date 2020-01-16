1b9984cacea58798606f3ee1c7f2b7aa2c97f19a
daniel <daniel.szarek98@gmail.com>
2020-01-16 16:12:13 +0100

    Possibility of adding user profile images #HAPP-48

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import include, url, static
+from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -27,4 +28,4 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # User images
