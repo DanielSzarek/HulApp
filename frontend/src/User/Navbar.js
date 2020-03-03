2c456df5f05e2fe2a5b58fa2aae9aa6c48ef9361
Justyna Brzezinska <brzezinska.j.uczelnia@gmail.com>
2020-03-03 19:31:50 +0100

    Basics of autoselect to search users #HAPP-69

--- frontend/src/User/Navbar.js
+++ frontend/src/User/Navbar.js
@@ -13,11 +13,18 @@ import AuthService from './AuthService';
 import logoNav from '../Images/logoNav.png';
 import '../Styles/Navbar.css';
 import { useHistory } from 'react-router-dom';
+import PersonAutoselect from '../User/PersonAutoselect';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 export default class Navbarex extends React.Component {
 
     constructor(props) {
             super(props);
+            this.state = {
+              user : '',
+              userCriteria: '',
+            }
 
             this.onClickLogOut = this.onClickLogOut.bind(this);
             this.Auth = new AuthService();
@@ -29,6 +36,16 @@ export default class Navbarex extends React.Component {
         window.location.reload();
 
     }
+
+     handleUserChange(val){
+		    console.log("user handleUserChange autocomplete changed "+val);
+		    this.setState({user: val})
+	}
+
+  userChanged(val){
+		console.log("user userChanged autocomplete changed "+val);
+
+	}
 
   render() {
     return (
@@ -45,9 +62,20 @@ export default class Navbarex extends React.Component {
                 Profil <FaUser />
               </NavLink>
             </NavItem>
+             <NavItem>
+                <PersonAutoselect
+                        controlId="formUsersSearch" 
+                        label="Przyjaciele" dest="users" 
+                        name="search" required="true" 
+                        onSelect={this.handleUserChange} 
+						            onChange={this.userChanged}
+                        value={this.state.user} 
+                        defVal={this.state.userCriteria}/>
+
+            </NavItem>
             <NavItem onClick={this.onClickLogOut}>
               <NavLink className="navLinkLogout" active href="/login">
-                Wyloguj <FiLogOut />
+               Wyloguj <FiLogOut />
               </NavLink>
             </NavItem>   
           </Nav>
---
+++ frontend/src/User/PersonAutoselect.js
@@ -0,0 +1,142 @@
+import React from 'react';
+// import '../Styles/UserProfile.css';
+import AutoComplete from 'react-autocomplete';
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from "@material-ui/core/InputAdornment";
+
+
+
+export default class PersonAutoselect extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            value: "",
+            autocompleteData: [],
+			selectedValue : ""
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSelect = this.onSelect.bind(this);
+        this.getItemValue = this.getItemValue.bind(this);
+        this.renderItem = this.renderItem.bind(this);
+        this.retrieveDataAsynchronously = this.retrieveDataAsynchronously.bind(this);
+    }
+
+
+    /**
+     * Updates the state of the autocomplete data with the remote data obtained via AJAX.
+     * 
+     * @param {String} searchText content of the input that will filter the autocomplete data.
+     * @return {Nothing} The state is updated but no value is returned
+     */
+    retrieveDataAsynchronously(searchText){
+        let _this = this;
+        let url = `http://hulapp.pythonanywhere.com/api/users/${searchText}`;
+        
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            let status = xhr.status;
+
+            if (status == 200) {
+                _this.setState({
+                    autocompleteData: xhr.response
+                });
+                console.log(xhr.response);
+            } else {
+                console.error("Cannot load data from remote source");
+            }
+        };
+
+        xhr.send();
+    }
+    
+    /**
+     * Callback triggered when the user types in the autocomplete field
+     * 
+     * @param {Event} e JavaScript Event
+     * @return {Event} Event of JavaScript can be used as usual.
+     */
+
+
+
+    onChange(e){
+        this.setState({
+           value: e.target.value
+        });
+
+        this.retrieveDataAsynchronously(e.target.value);
+
+    }
+
+    /**
+     * Callback triggered when the autocomplete input changes.
+     * 
+     * @param {Object} event Value returned by the getItemValue function.
+     * @return {Nothing} No value is returned
+     */
+    onSelect(event){
+        this.setState({
+            value: event.split("-")[1]
+        });
+		this.props.onSelect(event.split("-")[0]);
+    }
+
+	
+
+    /**
+     * Define the markup of every rendered item of the autocomplete.
+     * 
+     * @param {Object} item Single object from the data that can be shown inside the autocomplete
+     * @param {Boolean} isHighlighted declares wheter the item has been highlighted or not.
+     * @return {Markup} Component
+     */
+    renderItem(item, isHighlighted){
+        return (
+            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item.id}>
+                {item.name}
+            </div>   
+        ); 
+    }
+
+    /**
+     * Define which property of the autocomplete source will be show to the user.
+     * 
+     * @param {Object} item Single object from the data that can be shown inside the autocomplete
+     * @return {String} val
+     */
+    getItemValue(item){
+         return `${item.id} - ${item.name}`;
+   }
+
+    render() {
+        return (
+            <div className='form-group'>
+
+				
+				<AutoComplete
+					// placeholder={this.props.label}
+					inputProps={{ style: { width:'100%' ,border: '1px solid #ced4da', padding:'5px' },
+                    //  startAdornment:(<SearchIcon/>), 
+                    startAdornment : (
+                     <InputAdornment position={"start"}>
+                        {SearchIcon}
+                     </InputAdornment> ),
+                     placeholder:'Szukaj...'
+                     }}
+                    
+                    getItemValue={this.getItemValue}
+                    items={this.state.autocompleteData}
+                    renderItem={this.renderItem}
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    onSelect={this.onSelect}
+					name={this.props.name}
+                    // wrapperStyle={{ width:  '100%'   }}
+                />            
+            </div>
+        );
+    }
+}
