679bbd050d61cb8e568846149e6259be5601079a
daniel <daniel.szarek98@gmail.com>
2020-04-08 16:13:39 +0200

    Add new views for post to get whole CRUD with permissions #HAPP-92

--- backend/backend/post/models.py
+++ backend/backend/post/models.py
@@ -8,3 +8,9 @@ class Post(models.Model):
     mod_date = models.DateTimeField(auto_now_add=True)
     published = models.BooleanField(default=True)
     text = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return f"Post: {self.text} written by {self.author}"
--- backend/backend/post/urls.py
+++ backend/backend/post/urls.py
@@ -5,4 +5,5 @@ app_name = 'post'
 
 urlpatterns = [
     path('post/', views.PostView.as_view(), name="posts"),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_details')
 ]
