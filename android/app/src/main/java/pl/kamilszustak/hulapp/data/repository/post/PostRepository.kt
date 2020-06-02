d67ef36ef19cd6170d53cca96742c728fe94210e
Kamil Szustak <kamil.szustak98@gmail.com>
2020-06-02 11:10:56 +0200

    Add ability to add a post in background; #HAPP-114

--- android/app/src/main/java/pl/kamilszustak/hulapp/data/repository/post/PostRepository.kt
+++ android/app/src/main/java/pl/kamilszustak/hulapp/data/repository/post/PostRepository.kt
@@ -2,6 +2,8 @@ package pl.kamilszustak.hulapp.data.repository.post
 
 import android.app.Application
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.workDataOf
+import com.squareup.moshi.Moshi
 import kotlinx.coroutines.flow.Flow
 import pl.kamilszustak.hulapp.common.data.NetworkBoundResource
 import pl.kamilszustak.hulapp.common.data.NetworkCall
@@ -16,6 +18,7 @@ import pl.kamilszustak.hulapp.domain.model.network.EditPostRequestBody
 import pl.kamilszustak.hulapp.domain.model.post.PostJson
 import pl.kamilszustak.hulapp.domain.model.post.PostWithAuthorEntity
 import pl.kamilszustak.hulapp.network.ApiService
+import pl.kamilszustak.hulapp.util.adapter
 import retrofit2.Response
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -82,24 +85,20 @@ class PostRepository @Inject constructor(
     }
 
     suspend fun add(requestBody: AddPostRequstBody): Result<Unit> {
+        val moshiAdapter = Moshi.Builder()
+            .build()
+            .adapter<AddPostRequstBody>()
+
+        val json = moshiAdapter.toJson(requestBody)
+        val data = workDataOf(AddPostWorker.REQUEST_BODY_KEY to json)
+
         val request = OneTimeWorkRequestBuilder<AddPostWorker>()
+            .setInputData(data)
             .build()
         
         workManager.enqueue(request)
 
-        return object : NetworkCall<PostJson, Unit>() {
-            override suspend fun makeCall(): Response<PostJson> =
-                apiService.addPost(requestBody)
-
-            override suspend fun mapResponse(response: PostJson): Unit = Unit
-
-            override suspend fun saveCallResult(result: PostJson) {
-                userDao.insert(result.author)
-                postJsonMapper.onMap(result) { post ->
-                    postDao.insert(post)
-                }
-            }
-        }.callForResponse()
+        return Result.success(Unit)
     }
 
     suspend fun editById(id: Long, requestBody: EditPostRequestBody): Result<Unit> {
