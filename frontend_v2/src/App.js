1f1faa5265be011066fb94a7799feed9e6585749
Justyna Brzezinska <brzezinska.j.uczelnia@gmail.com>
2020-09-30 00:38:11 +0200

    "SASS added. Code cleaned"

--- frontend_v2/src/App.js
+++ frontend_v2/src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import './App.css';
-import FooterPage from './components/skeleton/footer'
-import NavbarPage from './components/skeleton/header'
-import CarousselPage from './components/home/gallery'
-import { Form, InputGroup, Container, Row, Col } from 'react-bootstrap'
-import husquad from './assets/husquad.jpg'
-import Info from './components/home/info'
+import "./sass/styles.css"
 
-
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-      <NavbarPage/>
+    <body>
+      <header>
+         <h1>new version</h1>
       </header>
-      <body>
-      <div>
-      <Info/>
-      {/* <img src={husquad}/>
-      <div>Czas poruszyć świat! Hulając przez miasta, badamy możliwości i promujemy Hybrydową Mobilność Miejską, a także podejmujemy kwestie innych aspektów i problemów urbanistyki i architektury, jak np. zrównoważony rozwój, przemiany rewitalizacyjne, architektura uniwersalna, kształtowanie tkanki w kontekście historii i współczesności! W poszukiwaniu najlepszych wzorców i doświadczeń podbijamy nie tylko Trójmiasto, ale także inne miasta Polski i Europy!</div> */}
-          <CarousselPage/>
-          </div>
-      </body>
-      <footer>
-      <FooterPage/>
-      </footer>
-    </div>
-  );
+      <main>
+      </main>
+    </body>
+  
+  )
 }
 
-export default App;
+export default App
--- frontend_v2/src/index.js
+++ frontend_v2/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
